211d61336be206455bf26ed67fb8755bd7efdc6a
abhishekpachkawade <abhishekpachkawade@gmail.com>
2021-07-23 18:44:14 +0530

    Added: calculate wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,13 @@
-#Welcome to 
+#!/bin/bash -x
+
+empCheck=$((RANDOM%2))
+isPresent=1
+
+if [ $empCheck -eq $isPresent ]
+then
+	empRatePerHr=20
+	empHrs=8
+	wage=$(($empRatePerHr*$empHrs))
+else
+	wage=0
+fi
